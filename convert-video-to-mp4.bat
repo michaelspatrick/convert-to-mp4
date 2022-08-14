902c088e5394e06aa4c977a26b0d08cbfd66afb1
Mike Patrick <45206934+michaelspatrick@users.noreply.github.com>
2022-08-14 12:00:47 -0400

    Update convert-video-to-mp4.bat

--- convert-video-to-mp4.bat
+++ convert-video-to-mp4.bat
@@ -21,14 +21,14 @@ for /L %%i in (1,1,%argCount%) do (
 
   title "Optimize Video for Web Streaming (!mp4!)"
 
-  :: %ffmpeg% -i !infile! -c:v copy -c:a aac -strict experimental -b:a 128k !mp4_tmp!
-  :: %ffmpeg% -y -i !infile! -strict experimental -pix_fmt yuv420p -profile:v baseline -level 3.0 -acodec aac -ar 44100 -ac 2 -ab 128k -vf "scale=720:480" -async 1 -movflags faststart !mp4_tmp!
-  %ffmpeg% -y -i !infile! -strict experimental -pix_fmt yuv420p -profile:v baseline -level 3.0 -acodec aac -ar 44100 -ac 2 -ab 128k -async 1 -movflags faststart !mp4_tmp! 
+  :: %ffmpeg% -i "!infile!" -c:v copy -c:a aac -strict experimental -b:a 128k "!mp4_tmp!"
+  :: %ffmpeg% -y -i "!infile!" -strict experimental -pix_fmt yuv420p -profile:v baseline -level 3.0 -acodec aac -ar 44100 -ac 2 -ab 128k -vf "scale=720:480" -async 1 -movflags faststart "!mp4_tmp!"
+  %ffmpeg% -y -i "!infile!" -strict experimental -pix_fmt yuv420p -profile:v baseline -level 3.0 -acodec aac -ar 44100 -ac 2 -ab 128k -async 1 -movflags faststart "!mp4_tmp!" 
   if errorlevel 1 goto :abort
 
-  del !mp4!
-  del !infile!
-  rename !mp4_tmp! !mp4!
+  del "!mp4!"
+  del "!infile!"
+  rename "!mp4_tmp!" "!mp4!"
   if errorlevel 1 goto :abort
   color A0 
   echo "Success!"
@@ -38,8 +38,6 @@ title "Process Complete!"
 color A0
 pause
 exit
-
-:: ---------------------------------------------------------------------------------------------------------------------
 
 :abort
 color CF
